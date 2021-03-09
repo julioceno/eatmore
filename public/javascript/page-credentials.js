182895000614d7399f04f53cc1deb3bc7c3bfa8f
julioceno <julioreplay@gmail.com>
2021-03-09 20:42:51 -0300

    pagina de entrar | registrar quase no fim

--- public/javascript/page-credentials.js
+++ public/javascript/page-credentials.js
@@ -11,10 +11,13 @@ function verifyOption() {
             .then(resp => resp.text())
             .then(html => section.innerHTML = html)
 
+
     } else {
         fetch("./options/register-se.html")
         .then(resp => resp.text())
         .then(html => section.innerHTML = html)
+    
+    
     }
 }
 
@@ -23,7 +26,6 @@ verifyOption()
 function showPassword(event) {
     const input = event.currentTarget.parentNode.children[0]
     const button = event.currentTarget
-    console.log()
     
     
     if (input.type === "password") {
