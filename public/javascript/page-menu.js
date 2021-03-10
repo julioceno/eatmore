2c9e2780a596b72ab031e6b62dc440e7b3f614a8
julioceno <julioreplay@gmail.com>
2021-03-10 17:33:04 -0300

    add animation

--- public/javascript/page-menu.js
+++ public/javascript/page-menu.js
@@ -8,6 +8,22 @@ function appearMenu() {
         sideMenu.classList.add('appear-menu-options')
         
     } else {
+
+       sideMenu.animate([
+           {transform: 'translateX(-200px)'}
+       ], {
+            duration: 250,
+       }
+    
+       )
+       setTimeout(() =>{
         sideMenu.classList.remove('appear-menu-options')
+        
+    }, 250)
     }
 }
+
+setTimeout(() =>{
+    console.log('ola')
+    
+}, 100)
