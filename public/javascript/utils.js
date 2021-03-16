3686cf6a7b8aab0179a8b50f171b711b3bdf600d
julioceno <julioreplay@gmail.com>
2021-03-16 19:37:19 -0300

    ajustando a barra lateral

---
+++ public/javascript/utils.js
@@ -0,0 +1,26 @@
+const sideMenu = document.querySelector('.menu-options')
+const checkbox = document.getElementById('check')
+
+function appearMenu() {
+    
+
+    if (!checkbox.checked) {
+        sideMenu.classList.add('appear-menu-options')
+        
+    } else {
+
+       sideMenu.animate([
+           {transform: 'translateX(-300px)'}
+       ], {
+            duration: 300,
+       }
+    
+       )
+       setTimeout(() =>{
+        sideMenu.classList.remove('appear-menu-options')
+        
+    }, 300)
+    }
+}
+
+
