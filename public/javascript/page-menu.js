b984a472aed01e8070aa3307279100af982115f0
julioceno <julioreplay@gmail.com>
2021-03-18 01:21:23 -0300

    seleção de categoria pronta

--- public/javascript/page-menu.js
+++ public/javascript/page-menu.js
@@ -36,8 +36,10 @@ const categories = document.querySelectorAll('.slider-categories .categories')
 
 const categorieHamburguer = document.querySelector('.slider-categories .categories') 
 const categoriePizza = document.querySelector('.slider-categories .categories:nth-child(2)') 
-
-console.log()
+const categorieChicken = document.querySelector('.slider-categories .categories:nth-child(3)') 
+const categorieFriedPastry = document.querySelector('.slider-categories .categories:nth-child(4)') 
+const categorieDrinks = document.querySelector('.slider-categories .categories:nth-child(5)') 
+const categorieFruits = document.querySelector('.slider-categories .categories:nth-child(6)') 
 
 function deselectingCategories() {
     categories.forEach( categorie => {
@@ -47,6 +49,10 @@ function deselectingCategories() {
 
     categorieHamburguer.children[0].children[0].src = "./public/images/categories/hamburguer.svg"
     categoriePizza.children[0].children[0].src = "./public/images/categories/pizza.svg"
+    categorieChicken.children[0].children[0].src = "./public/images/categories/chicken.svg"
+    categorieFriedPastry.children[0].children[0].src = "./public/images/categories/fried-pastry.svg"
+    categorieDrinks.children[0].children[0].src = "./public/images/categories/drinks.svg"
+    categorieFruits.children[0].children[0].src = "./public/images/categories/fruits.svg"
 
 }
 
@@ -73,3 +79,52 @@ categoriePizza.addEventListener("click", e => {
     e.preventDefault()
 })
 
+
+categorieChicken.addEventListener("click", e => {
+    deselectingCategories()
+
+    categorieChicken.classList.add('categorie-selected')
+
+
+    const img = categorieChicken.children[0].children[0]
+    img.src = "./public/images/categories/chicken-focus.svg"
+
+    e.preventDefault()
+})
+
+
+categorieFriedPastry.addEventListener("click", e => {
+    deselectingCategories()
+
+    categorieFriedPastry.classList.add('categorie-selected')
+
+
+    const img = categorieFriedPastry.children[0].children[0]
+    img.src = "./public/images/categories/fried-pastry-focus.svg"
+
+    e.preventDefault()
+})
+
+categorieDrinks.addEventListener("click", e => {
+    deselectingCategories()
+
+    categorieDrinks.classList.add('categorie-selected')
+
+
+    const img = categorieDrinks.children[0].children[0]
+    img.src = "./public/images/categories/drinks-focus.svg"
+
+    e.preventDefault()
+})
+
+
+categorieFruits.addEventListener("click", e => {
+    deselectingCategories()
+
+    categorieFruits.classList.add('categorie-selected')
+
+    const img = categorieFruits.children[0].children[0]
+    img.src = "./public/images/categories/fruits-focus.svg"
+
+    e.preventDefault()
+})
