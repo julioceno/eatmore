9b68d6b1cd979cb9a682d421cb0da9290c683c90
julioceno <julioreplay@gmail.com>
2021-04-04 22:42:14 -0300

    iniciando o food html

---
+++ public/javascript/page-food.js
@@ -0,0 +1,126 @@
+const foodsButtons = document.querySelectorAll('.images button')
+const imageMain = document.querySelector('.food main img')
+
+foodsButtons.forEach( button => {
+    
+    button.addEventListener('click', event => {
+        const currentImage = event.currentTarget
+        const src = event.target.src
+
+        foodsButtons.forEach( button => {
+            button.classList.remove('active')
+        })
+
+        currentImage.classList.add('active')
+
+        imageMain.src = src
+    })
+})
+
+function addFoodList() {
+    const id = 5
+
+
+    const allFoods = [
+        {
+            caregorie: 'hamburguer',
+            id: 1,
+            img: './public/images/hamburguer-example.svg' ,
+            name: 'Hamburgyu1',
+            rating: 3,
+            value: 8.50
+        },
+
+        {
+            caregorie: 'hamburguer',
+            id: 2,
+            img: './public/images/hamburguer-example.svg' ,
+            name: 'Pastel2',
+            rating: 3,
+            value: 10.00
+        },
+
+        {
+            caregorie: 'hamburguer',
+            id: 3,
+            img: './public/images/hamburguer-example.svg' ,
+            name: 'Salada de frutas3',
+            rating: 3,
+            value: 5.50
+        },
+
+        {
+            caregorie: 'hamburguer',
+            id: 4,
+            img: './public/images/hamburguer-example.svg' ,
+            name: 'Salada de frutas4',
+            rating: 3,
+            value: 5.50
+        },
+
+        {
+            caregorie: 'hamburguer',
+            id: 5,
+            img: './public/images/hamburguer-example.svg' ,
+            name: 'Salada de frutas5',
+            rating: 3,
+            value: 5.50
+        },
+        
+    ]
+
+    let idNewFood
+    
+    allFoods.forEach( (food, indice) => {
+        if (food.id === id) idNewFood = food
+    })
+
+
+    // Se o id que vier da url não for encontrado na lista de alimentos eu não vou 
+    // prosseguir com a execução da função
+    if (!idNewFood) return
+
+
+    const chosenFoods = JSON.parse(localStorage.chosenFoods)
+
+
+    // Aqui eu verifico se a comida que ele está escolhendo está no carrinho de compras
+    let foodAlreadyExsts
+    chosenFoods.forEach( food => {
+        if (food.id === id)  foodAlreadyExsts = true
+    })
+
+
+
+    let amountOfChosenFoodsUpdated 
+    if (foodAlreadyExsts) {
+        amountOfChosenFoodsUpdated = chosenFoods.map( (food, indice) => {
+            if (food.id === id) {
+                const newAmount = food.amount + 1
+                food.amount = newAmount
+            }
+
+            return food
+        })
+    } else {
+        amountOfChosenFoodsUpdated = chosenFoods.map( (food, indice) => {
+            if (food.id === id) {
+                const newAmount = food.amount + 1
+                food.amount = newAmount
+            }
+
+            return food
+        })
+
+        amountOfChosenFoodsUpdated.push({
+            id, 
+            amount: 1
+        })
+
+    }
+
+    console.log(chosenFoods)
+
+    localStorage.chosenFoods = JSON.stringify(amountOfChosenFoodsUpdated)
+
+}
--- public/javascript/utils.js
+++ public/javascript/utils.js
@@ -17,7 +17,7 @@ function appearMenu() {
        setTimeout(() =>{
         sideMenu.classList.remove('appear-menu-options')
         
-    }, 300)
+    }, 258)
     }
 }
 
@@ -34,17 +34,17 @@ const purchasingCar = document.querySelector('.purchasing-car')
 */
 const foodsSelectedsTest = [
     {
-        id: 1,
+        id: 2,
         amount: 3
     },
 
     {
-        id: 2,
+        id: 3,
         amount: 4
     },
 
     {
-        id: 3,
+        id: 4,
         amount: 1
     }    
 ]
@@ -53,114 +53,116 @@ localStorage.chosenFoods = JSON.stringify(foodsSelectedsTest)
 
 /*
 #####################################################################
-================ ATÉ AQUI ==========================================
+=========================== ATÉ AQUI ===============================
 #####################################################################
 */
 
-// Gerando alimentos no carrinho através do JS
 
-const containerChosenFoods = document.querySelector('.chosenFoods')
-const chosenFoods = JSON.parse(localStorage.chosenFoods)
 
-chosenFoods.forEach( ({ id, amount }) => {
-    const allFoods = [
-            {
-                caregorie: 'hamburguer',
-                id: 1,
-                img: './public/images/hamburguer-example.svg' ,
-                name: 'Hamburgyu',
-                rating: 3,
-                value: 8.50
-            },
 
-            {
-                caregorie: 'hamburguer',
-                id: 2,
-                img: './public/images/hamburguer-example.svg' ,
-                name: 'Pastel',
-                rating: 3,
-                value: 10.00
-            },
+function generateNewFoods() {
+    // Gerando alimentos no carrinho através do JS
+    const containerChosenFoods = document.querySelector('.chosenFoods')
+    let chosenFoods = JSON.parse(localStorage.chosenFoods) 
 
-            {
-                caregorie: 'hamburguer',
-                id: 3,
-                img: './public/images/hamburguer-example.svg' ,
-                name: 'Salada de frutas',
-                rating: 3,
-                value: 5.50
-            },
+    chosenFoods.forEach( ({ id, amount }) => {
+        const allFoods = [
+                {
+                    caregorie: 'hamburguer',
+                    id: 1,
+                    img: './public/images/hamburguer-example.svg' ,
+                    name: 'Hamburgyu',
+                    rating: 3,
+                    value: 8.50
+                },
+    
+                {
+                    caregorie: 'hamburguer',
+                    id: 2,
+                    img: './public/images/hamburguer-example.svg' ,
+                    name: 'Pastel',
+                    rating: 3,
+                    value: 10.00
+                },
+    
+                {
+                    caregorie: 'hamburguer',
+                    id: 3,
+                    img: './public/images/hamburguer-example.svg' ,
+                    name: 'Salada de frutas',
+                    rating: 3,
+                    value: 5.50
+                },
+                
+        ]
+    
+    
+        allFoods.forEach( food => {
+    
+            if (food.id === id) {
+               
+                const tagDivFood = document.createElement('div')
+                tagDivFood.classList.add('food')
+    
+                const inputHidden = document.createElement('input')
+                inputHidden.setAttribute('type', 'hidden')
+                inputHidden.setAttribute('name', 'id')
+                inputHidden.setAttribute('value', id)
             
-    ]
-
-
-    allFoods.forEach( food => {
-
-        if (food.id === id) {
-           
-            const tagDivFood = document.createElement('div')
-            tagDivFood.classList.add('food')
-
-            const inputHidden = document.createElement('input')
-            inputHidden.setAttribute('type', 'hidden')
-            inputHidden.setAttribute('name', 'id')
-            inputHidden.setAttribute('value', id)
+                const tagMain = document.createElement('main')
+            
+                const tagImg = document.createElement('img')
+                tagImg.src = food.img
         
-            const tagMain = document.createElement('main')
         
-            const tagImg = document.createElement('img')
-            tagImg.src = food.img
-    
-    
-            const tagH6 = document.createElement('h6')
-            const textNameFood = document.createTextNode(food.name)
-            tagH6.appendChild(textNameFood)
-    
-            const tagDivAmountThisFood = document.createElement('div')
-            tagDivAmountThisFood.classList.add('amount-this-food')
-    
-            const tagButtonLeft = document.createElement('button')
-    
-            const tagImgArrowLeft = document.createElement('img')
-            tagImgArrowLeft.src= "./public/images/amount-arrow-left.svg"
-    
-            const tagDivAmount = document.createElement('div')
-            tagDivAmount.classList.add('amount')
-            const textAmountFood = document.createTextNode(amount)
-            tagDivAmount.appendChild(textAmountFood)
-
-    
-            const tagButtonRight = document.createElement('button')
-    
-            const tagImgArrowRight = document.createElement('img')
-            tagImgArrowRight.src= "./public/images/amount-arrow-right.svg"
-    
-            tagDivFood.appendChild(inputHidden)
-
-            tagDivFood.appendChild(tagMain)
-            tagMain.appendChild(tagImg)
-            tagMain.appendChild(tagH6)
-    
-    
-            tagButtonLeft.appendChild(tagImgArrowLeft)
-            tagDivAmountThisFood.appendChild(tagButtonLeft)
-    
-            tagDivAmountThisFood.appendChild(tagDivAmount)
-    
-            tagButtonRight.appendChild(tagImgArrowRight)
-            tagDivAmountThisFood.appendChild(tagButtonRight)
-    
-            tagDivFood.appendChild(tagDivAmountThisFood)
-
-            containerChosenFoods.appendChild(tagDivFood)
-        }
-
+                const tagH6 = document.createElement('h6')
+                const textNameFood = document.createTextNode(food.name)
+                tagH6.appendChild(textNameFood)
         
+                const tagDivAmountThisFood = document.createElement('div')
+                tagDivAmountThisFood.classList.add('amount-this-food')
+        
+                const tagButtonLeft = document.createElement('button')
+        
+                const tagImgArrowLeft = document.createElement('img')
+                tagImgArrowLeft.src= "./public/images/amount-arrow-left.svg"
+        
+                const tagDivAmount = document.createElement('div')
+                tagDivAmount.classList.add('amount')
+                const textAmountFood = document.createTextNode(amount)
+                tagDivAmount.appendChild(textAmountFood)
+    
+        
+                const tagButtonRight = document.createElement('button')
+        
+                const tagImgArrowRight = document.createElement('img')
+                tagImgArrowRight.src= "./public/images/amount-arrow-right.svg"
+        
+                tagDivFood.appendChild(inputHidden)
+    
+                tagDivFood.appendChild(tagMain)
+                tagMain.appendChild(tagImg)
+                tagMain.appendChild(tagH6)
+        
+        
+                tagButtonLeft.appendChild(tagImgArrowLeft)
+                tagDivAmountThisFood.appendChild(tagButtonLeft)
+        
+                tagDivAmountThisFood.appendChild(tagDivAmount)
+        
+                tagButtonRight.appendChild(tagImgArrowRight)
+                tagDivAmountThisFood.appendChild(tagButtonRight)
+        
+                tagDivFood.appendChild(tagDivAmountThisFood)
+    
+                containerChosenFoods.appendChild(tagDivFood)
+            }
+    
+            
+        })
     })
+}
 
-
-
-})
 
 // Botões de aumentar e diminuir a  quantidade de alimentos do carrinho
 
@@ -174,7 +176,7 @@ buttonsDecreaseAmount.forEach( button => {
         const id = Number(button.currentTarget.parentNode.parentNode.children[0].value)
 
         const chosenFoods = JSON.parse(localStorage.chosenFoods)
-
+        // limpo o localstorage
         localStorage.clear()
 
         let indiceDeleteChosenFood;
@@ -204,7 +206,7 @@ buttonsDecreaseAmount.forEach( button => {
         localStorage.chosenFoods = JSON.stringify(amountOfChosenFoodsUpdated)
 
         calculatingValueTotal()
-        
+        generateNewFoods() 
     })
 })
 
@@ -219,6 +221,7 @@ buttonsIncreaseAmount.forEach( button => {
         const chosenFoods = JSON.parse(localStorage.chosenFoods)
 
        const amountOfChosenFoodsUpdated = chosenFoods.map( (food, indice) => {
+          
             if (food.id === id) {
                 const newAmount = food.amount + 1
 
@@ -234,6 +237,7 @@ buttonsIncreaseAmount.forEach( button => {
         localStorage.chosenFoods = JSON.stringify(amountOfChosenFoodsUpdated)
         
         calculatingValueTotal()
+        generateNew()
 
     })
 })
