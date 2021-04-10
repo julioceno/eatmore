1968065d400a23df1becc1ae69d5ea6df85b0260
julioceno <julioreplay@gmail.com>
2021-04-10 12:14:12 -0300

    page food terminada

--- public/javascript/page-food.js
+++ public/javascript/page-food.js
@@ -1,5 +1,103 @@
-const foodsButtons = document.querySelectorAll('.images button')
+const idFood = 1
+
 const imageMain = document.querySelector('.food main img')
+const foodName = document.querySelector('.food main div h4')
+const starRating = document.querySelector('.food main div .stars-outer .stars-inner')
+const foodsButtonsContainer = document.querySelector('.images')
+const foodsButtonsImgs = document.querySelectorAll('.images button img')
+const foodValue = document.querySelector('.food footer span')
+
+const allFoods = [
+    {
+        caregorie: 'hamburguer',
+        id: 1,
+        imgs: [
+            './public/images/hamburguer-example.svg',
+            './public/images/food-plate.svg',
+            './public/images/hamburguer-example.svg',
+            './public/images/food-plate.svg',
+        ] ,
+        name: 'hamburguer',
+        rating: 2,
+        value: 8.50,
+
+    },
+
+    {
+        caregorie: 'hamburguer',
+        id: 2,
+        img: './public/images/hamburguer-example.svg' ,
+        name: 'Pastel2',
+        rating: 3,
+        value: 10.00
+    },
+
+    {
+        caregorie: 'hamburguer',
+        id: 3,
+        img: './public/images/hamburguer-example.svg' ,
+        name: 'Salada de frutas3',
+        rating: 3,
+        value: 5.50
+    },
+
+    {
+        caregorie: 'hamburguer',
+        id: 4,
+        img: './public/images/hamburguer-example.svg' ,
+        name: 'Salada de frutas4',
+        rating: 3,
+        value: 5.50
+    },
+
+    {
+        caregorie: 'hamburguer',
+        id: 5,
+        img: './public/images/hamburguer-example.svg' ,
+        name: 'Salada de frutas5',
+        rating: 3,
+        value: 5.50
+    },
+]
+
+allFoods.forEach( food => {
+    if ( food.id === idFood ) {
+        const imgs = food.imgs
+        imageMain.src = imgs[0]
+        foodName.innerHTML = food.name
+
+        /* Adicionando os valores das estrelas */
+
+        // Get porcentage
+        const starPercentage = (food.rating / 5) * 100
+
+        // Round to nearest 10
+        const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`
+
+        // Set width of stars-inner to percentage
+        starRating.style.width = starPercentageRounded
+
+
+        imgs.forEach( (img, indice) => {
+            const elementButton = document.createElement('button')
+
+            const elementImg = document.createElement('img')
+            elementImg.src = img
+            elementImg.alt = `Imagem ${indice + 1}`
+
+
+            elementButton.appendChild(elementImg)
+            foodsButtonsContainer.appendChild(elementButton)
+        })
+
+        foodValue.innerHTML = `R$ ${food.value.toFixed(2).toString().replace('.', ',')}`
+
+    }
+})
+
+const foodsButtons = document.querySelectorAll('.images button')
+
+
 
 foodsButtons.forEach( button => {
     
@@ -18,55 +116,8 @@ foodsButtons.forEach( button => {
 })
 
 function addFoodList() {
-    const id = 2
+    const id = 1
 
-
-    const allFoods = [
-        {
-            caregorie: 'hamburguer',
-            id: 1,
-            img: './public/images/hamburguer-example.svg' ,
-            name: 'hamburguer',
-            rating: 3,
-            value: 8.50
-        },
-
-        {
-            caregorie: 'hamburguer',
-            id: 2,
-            img: './public/images/hamburguer-example.svg' ,
-            name: 'Pastel2',
-            rating: 3,
-            value: 10.00
-        },
-
-        {
-            caregorie: 'hamburguer',
-            id: 3,
-            img: './public/images/hamburguer-example.svg' ,
-            name: 'Salada de frutas3',
-            rating: 3,
-            value: 5.50
-        },
-
-        {
-            caregorie: 'hamburguer',
-            id: 4,
-            img: './public/images/hamburguer-example.svg' ,
-            name: 'Salada de frutas4',
-            rating: 3,
-            value: 5.50
-        },
-
-        {
-            caregorie: 'hamburguer',
-            id: 5,
-            img: './public/images/hamburguer-example.svg' ,
-            name: 'Salada de frutas5',
-            rating: 3,
-            value: 5.50
-        },
-    ]
 
 
     let idNewFood
@@ -144,9 +195,6 @@ function addFoodList() {
 
 // avaliação
 
-
-
-
 const userData = {
 
     avaliation: {
@@ -219,3 +267,130 @@ if (userData.avaliation.rating && userData.avaliation.text) {
         currentElement.style.height = height + "px"
     })
 }
+
+// Gerando comentários 
+const evalueationContainer = document.querySelector('.other-reviews .line-up-grid-reviews')
+
+const evaluations = [
+    {
+        profile: "./public/images/doka.png",
+        name: "Julin",
+        rating: 5,
+        date: "19/09/2021",
+        text: "Testando",
+    },
+
+    {
+        profile: "./public/images/doka.png",
+        name: "Sidoka",
+        rating: 1.5,
+        date: "209/05/2021",
+        text: "saopkdop dipsamoip dopsamndo osamndosa doipsanodp",
+    },
+
+    {
+        profile: "./public/images/doka.png",
+        name: "Gulio",
+        rating: 3,
+        date: "12/02/2021",
+        text: "saopkdop dipsamoip dopsamndo osamndosa doipsanodp",
+    },
+
+]
+
+evaluations.forEach( ({profile, name, rating, date, text}) => {
+
+//     <div class="evaluation-container">
+
+//     <header>
+//         <img src="./public/images/doka.png" alt="">
+//         <h6>Sidoka</h6>
+//     </header>
+
+//     <main>
+        
+//         <div class="line-up-star-rating-and-date">
+//             <div>
+//                 <div class="stars-outer">
+//                     <div class="stars-inner"></div>
+//                 </div>
+//             </div>
+
+//             <span>12/02/2021</span>
+//         </div>
+
+//        <p class="user-comment">
+//         saopkdop dipsamoip dopsamndo osamndosa doipsanodp
+       
+//        </p> 
+        
+//     </main>
+// </div>
+
+
+    const elementDivContainer = document.createElement('div')
+    elementDivContainer.classList.add('evaluation-container')
+
+    const elementHeader = document.createElement('header')
+    const elementImg = document.createElement('img')
+    elementImg.src = profile
+    elementImg.alt = `foto de perfil do ${name}`
+
+
+    const elementh6 = document.createElement('h6')    
+    const elementh6Text = document.createTextNode(name)    
+    elementh6.appendChild(elementh6Text)
+
+    const elementMain = document.createElement('main')
+    const elementDivLineUpStarRatingAndDate = document.createElement('div')
+    elementDivLineUpStarRatingAndDate.classList.add('line-up-star-rating-and-date')
+
+    const elementDivStarContainer = document.createElement('div')
+
+    const elementDivStarOuther = document.createElement('div')
+    elementDivStarOuther.classList.add('stars-outer')
+
+
+    const elementDivStarInner = document.createElement('div')
+    elementDivStarInner.classList.add('stars-inner')
+
+     /* Adicionando os valores das estrelas */
+
+        // Get porcentage
+        const starPercentage = (rating / 5) * 100
+
+        // Round to nearest 10
+        const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`
+
+        // Set width of stars-inner to percentage
+        elementDivStarInner.style.width = starPercentageRounded
+
+
+    const elementDivSpan = document.createElement('span')
+    const elementDivSpanText = document.createTextNode(date)
+    elementDivSpan.appendChild(elementDivSpanText)
+
+    const elementPComment = document.createElement('p')
+    elementPComment.classList.add('user-comment')
+
+    const elementPCommentText = document.createTextNode(text)
+    elementPComment.appendChild(elementPCommentText)
+
+
+    elementHeader.appendChild(elementImg)
+    elementHeader.appendChild(elementh6)
+    elementDivContainer.appendChild(elementHeader)
+
+    elementDivStarOuther.appendChild(elementDivStarInner)
+    elementDivStarContainer.appendChild(elementDivStarOuther)
+    elementDivLineUpStarRatingAndDate.appendChild(elementDivStarContainer)
+    elementDivLineUpStarRatingAndDate.appendChild(elementDivSpan)
+
+    elementMain.appendChild(elementDivLineUpStarRatingAndDate)
+    elementMain.appendChild(elementPComment)
+
+    elementDivContainer.appendChild(elementMain)
+
+
+    evalueationContainer.appendChild(elementDivContainer)
+})
