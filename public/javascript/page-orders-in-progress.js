d2d7431d5ea0a9153417c6f55669002f5ea9f202
julioceno <julioreplay@gmail.com>
2021-04-15 00:35:56 -0300

    contador de tempo aidiconado

--- public/javascript/page-orders-in-progress.js
+++ public/javascript/page-orders-in-progress.js
@@ -40,7 +40,100 @@ function showMore(event) {
         requestContainer.classList.remove('animate-request-show-less')
         
         
+        const allChosenFoods = document.querySelectorAll('.chosen-foods-the-order .order-food')
+        allChosenFoods.forEach( food => food.remove() )
 
+
+        chosenFoods.forEach( ({ id, amount }) => {
+            const allFoods = [
+                    {
+                        caregorie: 'hamburguer',
+                        id: 1,
+                        img: './public/images/hamburguer-example.svg' ,
+                        name: 'Hamburgyu',
+                        rating: 3,
+                        value: 8.50
+                    },
+        
+                    {
+                        caregorie: 'hamburguer',
+                        id: 2,
+                        img: './public/images/hamburguer-example.svg' ,
+                        name: 'Pastel',
+                        rating: 3,
+                        value: 10.00
+                    },
+        
+                    {
+                        caregorie: 'hamburguer',
+                        id: 3,
+                        img: './public/images/hamburguer-example.svg' ,
+                        name: 'Salada de frutas',
+                        rating: 3,
+                        value: 5.50
+                    },
+
+                    {
+                        caregorie: 'hamburguer',
+                        id: 4,
+                        img: './public/images/hamburguer-example.svg' ,
+                        name: '444',
+                        rating: 3,
+                        value: 5.50
+                    },
+
+                    {
+                        caregorie: 'hamburguer',
+                        id: 5,
+                        img: './public/images/hamburguer-example.svg' ,
+                        name: '555',
+                        rating: 3,
+                        value: 5.50
+                    },
+                    
+            ]
+        
+            allFoods.forEach( food => {
+                if (food.id === id) {
+                
+                    const tagDivFood = document.createElement('div')
+                    tagDivFood.classList.add('order-food')
+        
+                    
+                
+                    const tagMain = document.createElement('main')
+                
+                    const tagImg = document.createElement('img')
+                    tagImg.src = food.img
+            
+            
+                    const tagH6 = document.createElement('h6')
+                    const textNameFood = document.createTextNode(food.name)
+                    tagH6.appendChild(textNameFood)
+            
+                
+            
+                
+                    const tagDivAmount = document.createElement('div')
+                    tagDivAmount.classList.add('amount')
+                    const textAmountFood = document.createTextNode(amount)
+                    tagDivAmount.appendChild(textAmountFood)
+        
+            
+                    tagDivFood.appendChild(tagMain)
+                    tagMain.appendChild(tagImg)
+                    tagMain.appendChild(tagH6)
+            
+            
+                    tagDivFood.appendChild(tagDivAmount)
+        
+                    containerChosenFoods.appendChild(tagDivFood)
+                }
+                
+            })
+
+        })
+    
     } else {
         requestContainer.classList.remove('selected')
     
@@ -51,96 +144,20 @@ function showMore(event) {
 
         event.currentTarget.innerHTML = "Mostrar mais"
     }
-    
+}
 
-    chosenFoods.forEach( ({ id, amount }) => {
-        const allFoods = [
-                {
-                    caregorie: 'hamburguer',
-                    id: 1,
-                    img: './public/images/hamburguer-example.svg' ,
-                    name: 'Hamburgyu',
-                    rating: 3,
-                    value: 8.50
-                },
-    
-                {
-                    caregorie: 'hamburguer',
-                    id: 2,
-                    img: './public/images/hamburguer-example.svg' ,
-                    name: 'Pastel',
-                    rating: 3,
-                    value: 10.00
-                },
-    
-                {
-                    caregorie: 'hamburguer',
-                    id: 3,
-                    img: './public/images/hamburguer-example.svg' ,
-                    name: 'Salada de frutas',
-                    rating: 3,
-                    value: 5.50
-                },
+const startingMinutes = 10 
+let time = startingMinutes * 60
 
-                {
-                    caregorie: 'hamburguer',
-                    id: 4,
-                    img: './public/images/hamburguer-example.svg' ,
-                    name: '444',
-                    rating: 3,
-                    value: 5.50
-                },
+const countDown = document.getElementById('countDown')
+setInterval(updateCountDown, 1000) 
 
-                {
-                    caregorie: 'hamburguer',
-                    id: 5,
-                    img: './public/images/hamburguer-example.svg' ,
-                    name: '555',
-                    rating: 3,
-                    value: 5.50
-                },
-                
-        ]
-    
-        allFoods.forEach( food => {
-            if (food.id === id) {
-               
-                const tagDivFood = document.createElement('div')
-                tagDivFood.classList.add('order-food')
-    
-                
-            
-                const tagMain = document.createElement('main')
-            
-                const tagImg = document.createElement('img')
-                tagImg.src = food.img
-        
-        
-                const tagH6 = document.createElement('h6')
-                const textNameFood = document.createTextNode(food.name)
-                tagH6.appendChild(textNameFood)
-        
-               
-        
-             
-                const tagDivAmount = document.createElement('div')
-                tagDivAmount.classList.add('amount')
-                const textAmountFood = document.createTextNode(amount)
-                tagDivAmount.appendChild(textAmountFood)
-    
-        
-                tagDivFood.appendChild(tagMain)
-                tagMain.appendChild(tagImg)
-                tagMain.appendChild(tagH6)
-        
-        
-                tagDivFood.appendChild(tagDivAmount)
-    
-                containerChosenFoods.appendChild(tagDivFood)
-            }
-            
-        })
+function updateCountDown() {
+    const minutes = Math.floor( time / 60)
+    let seconds = time % 60
 
-    })
-    
+    seconds = seconds < 10 ? '0' + seconds : seconds
+
+    countDown.innerHTML = `${minutes}:${seconds}`
+    time--
 }
